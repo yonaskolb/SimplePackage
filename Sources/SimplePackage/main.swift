5a01fd85e27c45090056243685fda2ad073f698f
Yonas Kolb <yonaskolb@gmail.com>
2018-04-07 11:12:38 +1000

    4.0.0 add CLI commands for testing

--- Sources/SimplePackage/main.swift
+++ Sources/SimplePackage/main.swift
@@ -1,1 +1,41 @@
-print("3.0.0")
+
+import Foundation
+
+let args = Array(CommandLine.arguments.dropFirst())
+
+func ask() {
+    standardOut("Ask a question? (y/n)")
+    let line = readLine()
+    if line == "y" {
+        ask()
+    }
+}
+
+func printHello() {
+    standardOut("hello \(Date())")
+    sleep(2)
+    printHello()
+}
+
+func standardOut(_ string: String, terminator: String = "\n") {
+    guard let data = "\(string)\(terminator)".data(using: .utf8) else { return }
+    FileHandle.standardOutput.write(data)
+}
+
+func standardError(_ string: String, terminator: String = "\n") {
+    guard let data = "\(string)\(terminator)".data(using: .utf8) else { return }
+    FileHandle.standardError.write(data)
+}
+
+if args.first == "help" || args.first == "-h" || args.first == "--help" {
+    standardOut("commands:\n  input - asks for input\n  continue - continually prints\n  error - returns error\n  help - this help")
+} else if args.first == "input" {
+    ask()
+} else if args.first == "error" {
+    standardError("this is an error")
+    exit(1)
+} else if args.first == "continue" {
+    printHello()
+} else {
+    standardOut("4.0.0")
+}
